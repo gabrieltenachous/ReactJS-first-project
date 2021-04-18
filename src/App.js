f1ca22b1608e1eb5c5d954e97843b06560330c30
gabrieltenachous <58226659+gabrieltenachous@users.noreply.github.com>
2021-04-17 21:31:34 -0300

    Retirando props nas aplicaçoes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
-// import Greet from './components/Greet'
+import Greet from './components/Greet'
 // import Message from './components/Message'
-import Counter from './components/Counter'
-// import Welcome from './components/Welcome'
+// import Counter from './components/Counter'
+import Welcome from './components/Welcome'
 // import Hello from './components/Hello'
 function App() {
   return (
     <div className="App">
       {/* <Message/> */}
-      <Counter/>
-      {/* <Greet name="Bruce" heroName="Batman">
-       <p>This is children props</p></Greet>
+      {/* <Counter/> */}
+      <Welcome/>
+      <Greet name="Bruce" heroName="Batman"/>
+       {/* <p>This is children props</p></Greet>
       <Greet name="Clark" heroName="Superman"></Greet>
-      <Greet name="Diana" heroName="Wonder Woman"/> */}
+      <Greet name="Diana" heroName="Wonder Woman"/>  */}
     </div>
   );
 }
