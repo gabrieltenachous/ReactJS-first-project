c27d34084e6547df352b2a5f488de17ba2f96826
gabrieltenachous <58226659+gabrieltenachous@users.noreply.github.com>
2021-04-17 21:45:35 -0300

    Click Handler

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
-import Greet from './components/Greet'
+// import Greet from './components/Greet'
 // import Message from './components/Message'
 // import Counter from './components/Counter'
-import Welcome from './components/Welcome'
+import ClassClick from './components/ClassClick'
+// import Welcome from './components/Welcome'
 // import Hello from './components/Hello'
+// import FunctionClick from'./components/FunctionClick'
 function App() {
   return (
     <div className="App">
       {/* <Message/> */}
       {/* <Counter/> */}
-      <Welcome/>
-      <Greet name="Bruce" heroName="Batman"/>
+      {/* <Welcome/> */}
+      {/* <FunctionClick/> */}
+      <ClassClick/>
+      {/* <Greet name="Bruce" heroName="Batman"/> */}
        {/* <p>This is children props</p></Greet>
       <Greet name="Clark" heroName="Superman"></Greet>
       <Greet name="Diana" heroName="Wonder Woman"/>  */}
