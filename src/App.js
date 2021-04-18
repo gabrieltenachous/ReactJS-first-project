71473301221836944f60ce2b01b62025cae19b94
gabrieltenachous <58226659+gabrieltenachous@users.noreply.github.com>
2021-04-17 21:22:17 -0300

    SetState in react js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 // import Greet from './components/Greet'
-import Message from './components/Message'
+// import Message from './components/Message'
+import Counter from './components/Counter'
 // import Welcome from './components/Welcome'
 // import Hello from './components/Hello'
 function App() {
   return (
     <div className="App">
-      <Message/>
+      {/* <Message/> */}
+      <Counter/>
       {/* <Greet name="Bruce" heroName="Batman">
        <p>This is children props</p></Greet>
       <Greet name="Clark" heroName="Superman"></Greet>
