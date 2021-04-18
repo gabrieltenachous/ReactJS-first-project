097c77b8b9750e25224556ee0128f1e9ebb56beb
gabrieltenachous <58226659+gabrieltenachous@users.noreply.github.com>
2021-04-18 18:20:03 -0300

    Event Bind

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import './App.css';
 // import Greet from './components/Greet'
 // import Message from './components/Message'
 // import Counter from './components/Counter'
-import ClassClick from './components/ClassClick'
+// import ClassClick from './components/ClassClick'
+import EventBind from './components/EventBind'
 // import Welcome from './components/Welcome'
 // import Hello from './components/Hello'
 // import FunctionClick from'./components/FunctionClick'
 function App() {
   return (
     <div className="App">
+      <EventBind/>
       {/* <Message/> */}
       {/* <Counter/> */}
       {/* <Welcome/> */}
       {/* <FunctionClick/> */}
-      <ClassClick/>
+      {/* <ClassClick/> */}
       {/* <Greet name="Bruce" heroName="Batman"/> */}
        {/* <p>This is children props</p></Greet>
       <Greet name="Clark" heroName="Superman"></Greet>
